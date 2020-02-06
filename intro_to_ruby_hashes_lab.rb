c72d1bfee9525d7ffdfc0970c7188692f9559442
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-06 19:03:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def id_generator
   p random_id
 end
 
-def my_hash_creator(*key, value)
+def my_hash_creator(key:, value)
   # return a hash that includes the key and value parameters passed into this method
   my_hash = { :key => value}
   p my_hash
