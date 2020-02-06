97a8d95fa887cd29f90c3ca035aaa84e89e548b4
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-06 18:57:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_key = key.fetch(:key)
+  my_key = key.fetch(:my_key)
   my_hash = { my_key => value}
   p my_hash
 end
